a460a35cb37a45d730648f22fafa626a566ada87
Takahito Hashimoto <ta.li.ryu.1994@gmail.com>
2018-06-14 22:51:01 +0900

    [fix] 改行コード漏れを修正

--- proconTemplate/src/jp/co/unirita/procon/core/Display.java
+++ proconTemplate/src/jp/co/unirita/procon/core/Display.java
@@ -3,7 +3,7 @@ package jp.co.unirita.procon.core;
 public class Display {
 
 	public static void printErrorMessage(int resultCode, String header, String body) {
-		System.out.printf("ERR%03d [%s] %s", resultCode, header, body);
+		System.out.printf("ERR%03d [%s] %s\n", resultCode, header, body);
 	}
 
 	public static void printSuccessMessage(String header, String body) {
