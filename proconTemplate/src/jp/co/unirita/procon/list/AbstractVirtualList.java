d359d31dbc00d8ea03f9e3e2c71ed8b64f5cff54
Takahito Hashimoto <ta.li.ryu.1994@gmail.com>
2018-06-20 23:32:20 +0900

    [add] 仮想リストクラスの抽象クラス

---
+++ proconTemplate/src/jp/co/unirita/procon/list/AbstractVirtualList.java
@@ -0,0 +1,36 @@
+package jp.co.unirita.procon.list;
+
+public abstract class AbstractVirtualList<T> implements VirtualList<T> {
+
+	private long size;
+	
+	public AbstractVirtualList (long size) {
+		this.size = size;
+	}
+
+	@Override
+	public long size() {
+		return size;
+	}
+
+	@Override
+	public abstract long count(); 
+
+	@Override
+	public abstract T get(long idx);
+
+	@Override
+	public abstract T[] get(long startIdx, long endIdx);
+
+	@Override
+	public abstract void set(long idx, T value);
+
+	@Override
+	public abstract void set(long startIdx, long endIdx, T value);
+
+	@Override
+	public abstract void remove(long idx);
+
+	@Override
+	public abstract void remove(long startIdx, long endIdx);
+}
