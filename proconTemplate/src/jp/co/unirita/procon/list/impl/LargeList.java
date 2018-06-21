a8299a7497eba0e0feddedd405138efdf5164431
Takahito Hashimoto <ta.li.ryu.1994@gmail.com>
2018-06-21 22:53:21 +0900

    [fix] setで管理するようにする

--- proconTemplate/src/jp/co/unirita/procon/list/impl/LargeList.java
+++ proconTemplate/src/jp/co/unirita/procon/list/impl/LargeList.java
@@ -1,91 +1,112 @@
 package jp.co.unirita.procon.list.impl;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.TreeSet;
 
 import jp.co.unirita.procon.list.AbstractVirtualList;
 
 public class LargeList<T> extends AbstractVirtualList<T> {
-	
-	Map<Long, T> cellMap;
-	List<RangeCell<T>> rangeCellList;
 
-	public LargeList () {
-		this(1000000000000L);
+	TreeSet<RangeCell<T>> rangeCellSet;
+
+	public LargeList() {
+		this(1_000_000_000_000L);
 	}
-	
+
 	public LargeList(long size) {
 		super(size);
-		cellMap = new HashMap<>();
-		rangeCellList = new ArrayList<>();
+		rangeCellSet = new TreeSet<>();
 	}
 
 	@Override
 	public long count() {
-		// TODO Auto-generated method stub
-		return 0;
+		return rangeCellSet.stream()
+				.mapToLong(rs -> rs.getEndIdx() - rs.getStartIdx() + 1L)
+				.sum();
 	}
 
 	@Override
 	public T get(long idx) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public T[] get(long startIdx, long endIdx) {
-		// TODO Auto-generated method stub
-		return null;
+		Object[] arr = new Object[(int) (endIdx - startIdx) + 1];
+		for (int i = 0; i <= endIdx - startIdx; i++) {
+			arr[i] = this.get(startIdx + i);
+		}
+		return (T[]) arr;
 	}
 
 	@Override
-	public void set(long idx, T value) {
-		// TODO Auto-generated method stub
-		
+	public void add(long idx, T value) {
+		this.add(idx, idx, value);
 	}
 
 	@Override
-	public void set(long startIdx, long endIdx, T value) {
-		// TODO Auto-generated method stub
+	public void add(long startIdx, long endIdx, T value) {
 		
 	}
 
 	@Override
 	public void remove(long idx) {
-		// TODO Auto-generated method stub
-		
+		this.remove(idx, idx);
 	}
 
 	@Override
 	public void remove(long startIdx, long endIdx) {
-		// TODO Auto-generated method stub
 		
 	}
-	
-	public static class RangeCell<T> {
-		long startIdx;
-		long endIdx;
-		T value;
+
+	public static class RangeCell<T> implements Comparable<RangeCell<T>>{
+		private long startIdx;
+		private long endIdx;
+		private T value;
 		
+		public RangeCell(long startIdx, long endIdx) {
+			this(startIdx, endIdx, null);
+		}
+
 		public RangeCell(long startIdx, long endIdx, T value) {
 			this.startIdx = startIdx;
 			this.endIdx = endIdx;
 			this.value = value;
 		}
+
+		@Override
+		public int hashCode() {
+			return 0;
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public boolean equals(Object obj) {
+			if (!(obj instanceof RangeCell)) {
+				return false;
+			}
+			RangeCell<T> rs = (RangeCell<T>) obj;
+			return rs.getStartIdx() == this.startIdx && rs.getEndIdx() == this.endIdx;
+		}
+		
+		@Override
+		public int compareTo(RangeCell<T> rs) {
+			if(rs.getStartIdx() == this.startIdx) {
+				return this.startIdx  < rs.getStartIdx() ? 1 : -1;
+			}
+			return this.endIdx < rs.getEndIdx() ? 1 : -1;
+		}
 		
 		public long getStartIdx() {
 			return this.startIdx;
 		}
-		
+
 		public long getEndIdx() {
 			return this.endIdx;
 		}
-		
+
 		public T getValue() {
 			return this.value;
 		}
 	}
-
 }
