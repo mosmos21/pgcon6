601af7597cc4b7a5ede703aa8ff3ffeecbab726a
Takahito Hashimoto <takahito_hashimoto@unirita.co.jp>
2018-06-22 09:22:53 +0900

    [fix] 古い奴だった

--- General/D/Main.java
+++ General/D/Main.java
@@ -6,9 +6,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] ss = br.readLine().split(" ");
-        long A = Long.parseLong(ss[0]), B = Long.parseLong(ss[1]), K = Long.parseLong(ss[2]);
-        long a = (A + K - 1) / K;
-        long b = (B + K - 1) / K;
+        long A = Long.parseLong(ss[0]);
+        long B = Long.parseLong(ss[1]);
+        long K = Long.parseLong(ss[2]);
+
+        long a = (A >= 0 ? (A + K - 1) / K : A / K);
+        long b = (B >= 0 ? (B + K - 1) / K : B / K);
         long ans = K * ((b - 1) * b / 2 - (a - 1) * a / 2);
         System.out.println(ans);
     }
