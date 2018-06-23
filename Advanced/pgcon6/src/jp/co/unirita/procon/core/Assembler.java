d280d8f84811983cfa5cee81ef684891ea0edacd
Takahito Hashimoto <ta.li.ryu.1994@gmail.com>
2018-06-23 13:17:34 +0900

    [fix] 行番号がコメントでずれるのを修正

--- Advanced/pgcon6/src/jp/co/unirita/procon/core/Assembler.java
+++ Advanced/pgcon6/src/jp/co/unirita/procon/core/Assembler.java
@@ -23,22 +23,22 @@ public class Assembler {
 			assembler = new Assembler();
 		}
 		try {
-			end = assembler.eval(assembler.load(is));
+			end = assembler.evalAll(assembler.load(is));
 		} catch (CommandExecException e) {
 			Display.printResult(e.getResult());
 		} catch (IOException e) {
 			e.printStackTrace();
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return end - start;
 	}
-	
-	private Assembler () {
+
+	private Assembler() {
 	}
 
-	private List<String[]> load(InputStream is) throws CommandExecException, IOException, Exception {		
-		List<String[]> list = new ArrayList<>();
+	private List<CmdRow> load(InputStream is) throws CommandExecException, IOException, Exception {
+		List<CmdRow> list = new ArrayList<>();
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(is));
 		String line = null;
@@ -53,29 +53,25 @@ public class Assembler {
 				cmdArr = line.split("[\\s]+");
 				cmdArr[0] = cmdArr[0].toUpperCase();
 			}
-			if(cmdArr[0].equals("ST")) {
-					new CommandST(row).execute(Arrays.copyOfRange(cmdArr, 1, cmdArr.length));
-			}else {
-				list.add(cmdArr);
+			if (cmdArr[0].equals("ST")) {
+				eval(row, cmdArr);
+			} else {
+				list.add(new CmdRow(row, cmdArr));
 			}
 		}
 		br.close();
 		return list;
 	}
 
-	private long eval(List<String[]> cmdList) {
-		for (int row = 1; row <= cmdList.size(); ++row) {
-			String[] cmdArr = cmdList.get(row - 1);
+	private long evalAll(List<CmdRow> cmdRowList) {
+		for (CmdRow cmdRow : cmdRowList) {
+			String[] cmdArr = cmdRow.getCmdArr();
 			if (isBrankOrComment(cmdArr[0])) {
 				continue;
 			}
-			String cmd = cmdArr[0];
-			String[] args = Arrays.copyOfRange(cmdArr, 1, cmdArr.length);
 			try {
-				Class<?> clazz = Class.forName("jp.co.unirita.procon.command.impl.Command" + cmd);
-				Command command = (Command) clazz.getConstructor(int.class).newInstance(row + 1);
-				Result success = command.execute(args);
-				Display.printResult(success);
+				Result result = eval(cmdRow.getRow(), cmdArr);
+				Display.printResult(result);
 			} catch (CommandExecException e) {
 				Display.printResult(e.getResult());
 			} catch (Exception e) {
@@ -85,7 +81,34 @@ public class Assembler {
 		return System.currentTimeMillis();
 	}
 
+	private Result eval(int row, String[] cmdArr) throws ClassNotFoundException, CommandExecException, Exception {
+		String cmd = cmdArr[0];
+		String[] args = Arrays.copyOfRange(cmdArr, 1, cmdArr.length);
+
+		Class<?> clazz = Class.forName("jp.co.unirita.procon.command.impl.Command" + cmd);
+		Command command = (Command) clazz.getConstructor(int.class).newInstance(row + 1);
+		return command.execute(args);
+	}
+
 	private boolean isBrankOrComment(String line) {
 		return line.equals("") || line.startsWith("#");
+	}
+
+	public static class CmdRow {
+		int row;
+		String[] cmdArr;
+
+		public CmdRow(int row, String[] cmdArr) {
+			this.row = row;
+			this.cmdArr = cmdArr;
+		}
+
+		public int getRow() {
+			return this.row;
+		}
+
+		public String[] getCmdArr() {
+			return this.cmdArr;
+		}
 	}
 }
